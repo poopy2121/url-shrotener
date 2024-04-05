a78c896a526b5bf5afc4da4dbfcb0d27148b536e
poopy2121 <ggg1111g1g1g@gmail.com>
2024-04-05 19:42:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const submit = document.getElementById("submit")
+
+submit.addEventListener('click', function()
+
+    {
+        const userurl = document.getElementById("inputfield").value
+        const shorturl = userurl.length
+        console.log(shorturl)
+
+    }
+)
+
+
+
+ 
